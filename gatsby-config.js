0a99f49fbe9068dab9d04065a14e7f518265aa31
Zsolt Szakal <zsltszakal@gmail.com>
2021-03-13 18:12:37 +0000

    Blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,13 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
- module.exports = {
-  plugins: [`gatsby-plugin-netlify-cms`],
+module.exports = {
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/blog`,
+        name: `markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`
+  ]
 }
